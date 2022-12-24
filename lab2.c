b64a0f51d9e6371f5b27d073f9b3aa3c363f136b
VadKempn <90304716+VadKempn@users.noreply.github.com>
2022-12-24 22:05:33 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int lab2 () {
-    float z1, z2, a;
+float calculateZ1(float a) {
+    return powf(2*sin(3*M_PI-2*a), 2)*powf(cos(5*M_PI+2*a), 2);
+}
+
+float calculateZ2(float a) {
+    return 0.25 - ((0.25) * sin(2.5 * M_PI - (8 * a)));
+}
+
+int main() {
+    float a;
     scanf("%f", &a);
-    z1 = powf(2*sin(3*M_PI-2*a), 2)*powf(cos(5*M_PI+2*a), 2);
+    float z1 = calculateZ1(a);
     printf("%f", z1);
     scanf("%f", &a);
-    z2 = 0.25 - ((0.25) * sin(2.5 * M_PI - (8 * a)));
+    float z2 = calculateZ2(a);
     printf("%f", z2);
 
     return 0;
-
 }
